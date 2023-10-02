5e8ed0fee71500c87939c9f343f8386426eabd18
Luthfi Daffa Prabowo <daffaprabowo5@gmail.com>
2023-10-02 17:44:57 +0700

    implementation nav component

--- app/src/main/java/com/dapascript/foodipedia/presentation/MainActivity.kt
+++ app/src/main/java/com/dapascript/foodipedia/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.dapascript.foodipedia
+package com.dapascript.foodipedia.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dapascript.foodipedia.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
